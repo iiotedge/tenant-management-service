a5901ab9a6fd9ac62c44afdc0e2bf1bf426aa6e1
S-8506 <s.8506.a@gmail.com>
2025-04-02 22:30:44 +0530

    Move eureka client and spring cloud config to parent pom. Refactor package name.

--- src/main/java/com/iotmining/services/tms/TenantManagementServiceApplication.java
+++ src/main/java/com/iotmining/services/tms/TenantManagementServiceApplication.java
@@ -1,4 +1,4 @@
-package com.iotmining.tms;
+package com.iotmining.services.tms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/iotmining/services/tms/TenantManagementServiceApplicationTests.java
+++ src/test/java/com/iotmining/services/tms/TenantManagementServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iotmining.tms;
+package com.iotmining.services.tms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
